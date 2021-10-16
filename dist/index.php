ee30e4926ce31441222951f5a1d852caa9ca2de7
Alexander Sabanov <alexsab@mail.ru>
2021-10-16 18:38:19 +0400

    work

--- dist/index.php
+++ dist/index.php
@@ -4,18 +4,60 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
+	<link rel="stylesheet" href="dist/app.css">
 </head>
 <body>
 
-<div x-data="{ open: false }">
-	<button @click="open = ! open">Expand</button>
+<form x-data="arrCars" x-init="$el.classList.remove('hidden')" class="hidden">
+	<button type="button" @click="fill()">Fill</button>
+	<br>
+	<select class="model" x-on:change="model=$event.target.value">
+		<option selected disabled>Модель</option>
+		<template x-for="item in models" :key="item">
+			<option :value="item" x-text="item"></option>
+		</template>
+	</select>
+	<select class="year" x-show="model" x-on:change="year=$event.target.value">
+		<option selected disabled>Год производства</option>
+		<template x-for="item in years" :key="item">
+			<option :value="item" x-text="item"></option>
+		</template>
+	</select>
+	<select class="volume" x-show="year" x-on:change="volume=$event.target.value">
+		<option selected disabled>Рабочий объем</option>
+		<template x-for="item in volumes" :key="item">
+			<option :value="item" x-text="item"></option>
+		</template>
+	</select>
+	<select class="type" x-show="volume" x-on:change="type=$event.target.value">
+		<option selected disabled>Тип двигателя</option>
+		<template x-for="item in types" :key="item">
+			<option :value="item" x-text="item"></option>
+		</template>
+	</select>
+	<br>
+	<ul x-show="type" >
+		<template x-for="item in prices" :key="item">
+			<li x-text="item"></li>
+		</template>
+	</ul>
+</form>
 
-	<span x-show="open">
-		Content...
+<hr>
+
+<button x-data @click="$store.darkMode.toggle()">Toggle Dark Mode</button>
+<div x-data :class="$store.darkMode.on && 'bg-black'">
+	<span x-show="$store.darkMode.on">
+		On
+	</span>
+	<span x-show="!$store.darkMode.on">
+		Ne On
 	</span>
 </div>
 
-	<script src="//unpkg.com/alpinejs" defer></script>
-	<script src="app.js"></script>
+
+	<!-- <script src="//unpkg.com/alpinejs" defer></script> -->
+	<!-- <script src="dist/alpine.js" defer></script> -->
+	<script src="dist/app.js"></script>
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -4,14 +4,16 @@ require('laravel-mix-serve');
 
 mix
 	.js('src/app.js', 'dist')
-	.setPublicPath('dist')
-	.serve('php -S localhost:8080 -t dist')
+	.sass('src/app.sass', 'dist')
+	// .setPublicPath('dist')
+	// .serve('php -S localhost:8080 -t dist')
 
 if (mix.inProduction()) {
 	mix.version();
 } else {
 	mix.sourceMaps().webpackConfig({ devtool: 'inline-source-map' });
 	mix.browserSync({
-		proxy: 'localhost:8080'
+		// proxy: 'localhost:8080'
+		proxy: 'http://alpine-test.test/dist'
 	});
 }
